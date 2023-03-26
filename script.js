524d7de3b72663f36ebb0278f7a4de046006c79a
Aandi1989 <navaliny1989@gmail.com>
2023-03-26 18:45:15 +0300

    pagination,navigation,bullets,fractions,progresbar

--- script.js
+++ script.js
@@ -8,4 +8,30 @@ new Swiper('.image-slider',{
         nextEl:'.swiper-button-next',
         prevEl:'.swiper-button-prev'
     },
+    // Навигация
+    // Буллеты, текущее положение, прогрессбар
+    pagination:{
+        el:'.swiper-pagination',
+        /* 
+        // Буллеты
+        clickable: true, // возможность пролистывать по клику на сам булетт
+        dynamicBullets:true, // кастомные буллеты меняющее размер взависимости от расстояния до активного элемента
+        renderBullet: function(index, className){ // выводим номер слайда или группы слайдов
+            return '<span class=" ' + className + ' ">' + (index + 1) + '</span>' ;
+        },
+        */
+
+       /* 
+       // Фракции  
+       type: 'fraction',
+       // Кастомный вывод фракций (добавляем к цифрам несколько слов)
+       renderFraction: function(currentClass, totalClass){
+        return 'Фото <span class="' + currentClass + '"></span>' + ' из ' + 
+        '<span class="' + totalClass + '"></span>';
+       },
+        */
+
+       // Прогрессбар
+       type:'progressbar'
+    },
 });
